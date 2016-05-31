05a708aa5200109bfe7945509a4fd9bdf5a8ed0c
tailmse02457 <tailmse02457@fpt.edu.vn>
2016-06-01 01:14:25 +0700

    TaiLM

--- OneVietnam/App_Start/Startup.Auth.cs
+++ OneVietnam/App_Start/Startup.Auth.cs
@@ -15,7 +15,7 @@ namespace OneVietnam
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context, user manager and signin manager to use a single instance per request
+            // Configure the db context, user manager and role manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationIdentityContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
