1702cae471e204c12dddf288fde5edd048832b08
tailmse02457 <tailmse02457@fpt.edu.vn>
2016-06-01 22:23:20 +0700

    Add RoleStoreDAO

---
+++ OneVietnam/DAL/RoleStoreDAO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Security;
+using AspNet.Identity.MongoDB;
+using Microsoft.AspNet.Identity;
+using MongoDB.Driver;
+using OneVietnam.DTL;
+
+namespace OneVietnam.DAL
+{
+    public class RoleStore : RoleStore<IdentityRole>
+    {
+        private readonly IMongoCollection<IdentityRole> _roles;
+        public RoleStore(IMongoCollection<IdentityRole> roles) : base(roles)
+        {
+            _roles = roles;
+        }
+        public Task<List<IdentityRole>> AllRolesAsync()
+        {
+            return _roles.Find(r => true).ToListAsync();
+        }
+    }
+}
