76d26eb8cdd8bec084d7074b10148213c1667b40
tailmse02457 <tailmse02457@fpt.edu.vn>
2016-06-01 22:18:35 +0700

    Add RolesManager

---
+++ OneVietnam/BLL/RolesManagerBLO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using AspNet.Identity.MongoDB;
+
+namespace OneVietnam.BLL
+{
+    public partial class ApplicationRoleManager
+    {
+        public async Task<List<IdentityRole>> AllRolesAsync()
+        {
+            return await _roleStore.AllRolesAsync();
+        }
+    }
+}
