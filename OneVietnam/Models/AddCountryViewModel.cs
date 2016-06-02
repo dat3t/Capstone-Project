a62a08fa86bdbe9c7f80421fd52a7ab2f78c4146
tailmse02457 <tailmse02457@fpt.edu.vn>
2016-06-03 00:01:24 +0700

    Add Country Function for TraNT

---
+++ OneVietnam/Models/AddCountryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OneVietnam.Models
+{
+    public class AddCountryViewModel
+    {
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+    }
+}
