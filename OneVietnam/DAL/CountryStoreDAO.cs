c0ce3e75a0a2b24fb690847c2bfee01c2baf03d7
tailmse02457 <tailmse02457@fpt.edu.vn>
2016-06-02 21:49:30 +0700

    Add Country Manager

---
+++ OneVietnam/DAL/CountryStoreDAO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using MongoDB.Driver;
+using OneVietnam.DTL;
+
+namespace OneVietnam.DAL
+{
+    public class CountryStore
+    {
+        private readonly IMongoCollection<Country> _countries;
+
+        public CountryStore(IMongoCollection<Country> countries)
+        {
+            _countries = countries;
+        }
+
+        public Task CreatAsync(Country country)
+        {
+            return _countries.InsertOneAsync(country);
+        }
+        public virtual Task UpdateAsync(Country country)
+        {
+            return (Task)_countries.ReplaceOneAsync<Country>((Expression<Func<Country, bool>>)(c => c.Id == country.Id), country, (UpdateOptions)null);
+        }
+    }
+}
---
+++ OneVietnam/DTL/CountryDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace OneVietnam.DTL
+{
+    public class Country
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; private set; }
+
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+
+        public Country(string name, string code)
+        {
+            Id = ObjectId.GenerateNewId().ToString();
+            CountryName = name;
+            CountryCode = code;
+        }
+    }
+}
