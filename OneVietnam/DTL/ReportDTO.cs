9f908f5c1c0199b661b9f1f748964f06c8260d63
ThamDTH <truongthiphuongdu@gmail.com>
2016-06-09 22:44:54 +0700

    ThamDTH

---
+++ OneVietnam/DTL/ReportDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using AspNet.Identity.MongoDB;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using OneVietnam.Models;
+
+namespace OneVietnam.DTL
+{
+    public class Report
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; private set; }
+
+        [BsonIgnoreIfNull]
+        public string PostId { get; set; }
+
+        [BsonIgnoreIfNull]
+        public string PostTitle { get; set; }
+
+        public string Username { get; set; }
+
+        public string ReportDescription { get; set; }
+
+        public bool ReportStatus { get; set; }
+    }
+}
