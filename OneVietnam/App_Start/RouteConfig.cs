a259c568a759a818c4f6dd30f17ccdcfe6df37fb
tailmse02457 <tailmse02457@fpt.edu.vn>
2016-07-12 01:10:49 +0700

    Tailm  : fix bug

--- OneVietnam/App_Start/RouteConfig.cs
+++ OneVietnam/App_Start/RouteConfig.cs
@@ -10,9 +10,9 @@ namespace OneVietnam
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
-        {
+        {           
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapRoute("PostList", "", new { controller = "Manage", action = "GetPosts" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
