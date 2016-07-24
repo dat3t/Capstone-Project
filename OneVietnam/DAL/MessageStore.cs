f86ed9a560ce19ec8af528e26a74041890b997fb
tailmse02457 <tailmse02457@fpt.edu.vn>
2016-07-24 11:19:11 +0700

    Add Message Collection

---
+++ OneVietnam/DAL/MessageStore.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using MongoDB.Driver;
+using OneVietnam.DTL;
+
+namespace OneVietnam.DAL
+{
+    public class MessageStore:AbstractStore<Message>
+    {
+        public MessageStore(IMongoCollection<Message> collection) : base(collection)
+        {
+
+        }
+
+        public override async Task UpdateAsync(Message instance)
+        {
+            try
+            {
+                await Collection.ReplaceOneAsync(m => m.Id == instance.Id, instance, null).ConfigureAwait(false);
+            }
+            catch (MongoConnectionException ex)
+            {
+                throw new MongoConnectionException(ex.ConnectionId, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public override async Task UpdateAsync(Message instance, bool upsert)
+        {
+            try
+            {
+                var option = new UpdateOptions() { IsUpsert = upsert };
+                await Collection.ReplaceOneAsync(p => p.Id == instance.Id, instance, option).ConfigureAwait(false);
+            }
+            catch (MongoConnectionException ex)
+            {
+                throw new MongoConnectionException(ex.ConnectionId, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
