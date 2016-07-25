11ab24a6aa39bf432439e327904579b2656356de
Dat3T <clever_sheep_93@yahoo.com>
2016-07-26 01:08:55 +0700

    add updateCurrentLocation() to timeline.js , add code to Cancel Button in _EditProfile

--- OneVietnam/Scripts/timeline.js
+++ OneVietnam/Scripts/timeline.js
@@ -1,4 +1,4 @@
-﻿    
+﻿
 function editableForm() {
     $('.tog').toggleClass('disabled');
     $('#btnSave').toggleClass('hides');
@@ -14,7 +14,7 @@ function submitEditProfile() {
     var oldName = document.getElementById("lblTimeLineUserName");
     var oldHeaderName = document.getElementById("txtHeaderUserName");
     var currentName = document.getElementById("UserName");
-    
+
     oldName.innerText = currentName.value;
     oldHeaderName.innerText = currentName.value;
 }
@@ -22,10 +22,11 @@ function submitEditProfile() {
 function cancelEditProfile() {
     $.ajax({
         type: 'GET',
-        url: 'EditProfile',        
-        success: function (partialResult) {            
+        url: 'EditProfile',
+        success: function (partialResult) {
             $("#EditProfileForm").html("");
             $("#EditProfileForm").html(partialResult);
+            showUserMarkerOnMap(x, y, address);
         }
     });
 }
@@ -35,21 +36,21 @@ function changeTwoFactorAuthentication() {
     $.ajax({
         type: 'POST',
         url: 'ChangeTwoFactorAuthentication',
-        data:{'value':param},
-        success: function() {                           
+        data: { 'value': param },
+        success: function () {
         }
     });
 }
 
-function showChangePasswordForm() {    
+function showChangePasswordForm() {
     $.ajax({
         type: 'GET',
-        url: 'ChangePassword',       
+        url: 'ChangePassword',
         success: function (partialResult) {
             $("#ChangePasswordForm").html(partialResult);
             $("#ShowPassword").html("");
             $("#btnChangePass").hide();
-        }        
+        }
     });
 }
 
@@ -62,37 +63,23 @@ function cancelChangePassword() {
 
 function closeChangePasswordForm() {
     var errorMsg = document.querySelector(".validation-summary-errors");
-    if (errorMsg === null)
-    {
+    if (errorMsg === null) {
         $("#ChangePasswordForm").html("");
         $("#ShowPassword").html("Thay đổi mật khẩu thường xuyên để nâng cao bảo mật hơn");
         $("#btnChangePass").show();
     }
-    
-}
-
-function updateCurrentLocation(x,y,address) {
-    var addr = document.getElementById("Location_Address");
-    var xcoordinate = document.getElementById("Location_XCoordinate");
-    var ycoordinate = document.getElementById("Location_YCoordinate");
-
-    
-
-    addr.value = "";
-    xcoordinate.value = "";
-    ycoordinate.value = "";
 
 }
 
-function showSetPasswordForm() {    
+function showSetPasswordForm() {
     $.ajax({
         type: 'GET',
-        url: 'SetPassword',               
+        url: 'SetPassword',
         success: function (partialResult) {
             $("#ChangePasswordForm").html(partialResult);
             $("#ShowPassword").html("");
             $("#btnChangePass").hide();
-        }        
+        }
     });
 }
 
@@ -102,7 +89,7 @@ function cancelSetPassword() {
     $("#btnChangePass").show();
 }
 
-function closeSetPasswordForm() {  
+function closeSetPasswordForm() {
     var errorMsg = document.querySelector(".validation-summary-errors");
     if (errorMsg === null) {
         $("#ChangePasswordForm").html("");
@@ -116,9 +103,9 @@ function closeSetPasswordForm() {
     }
 }
 
-function showUserMarkerOnMap(x,y,address) {
+function showUserMarkerOnMap(x, y, address) {
     //Declare icon for userLocationmarker
-    var icon = {
+    icon = {
         url: "../Content/Icon/location.png",
         size: new google.maps.Size(71, 71),
         origin: new google.maps.Point(0, 0),
@@ -127,12 +114,12 @@ function showUserMarkerOnMap(x,y,address) {
     };
 
     // Declare a myLocation marker using icon declared above, and bind it to the map
-    var userLocationMarker = new google.maps.Marker({
+    userLocationMarker = new google.maps.Marker({
         title: address,
         icon: icon
     });
-    var map = new google.maps.Map(document.getElementById('divShowMap'), {
-        center: { lat: x, lng: y},
+    map = new google.maps.Map(document.getElementById('divShowMap'), {
+        center: { lat: x, lng: y },
         zoom: 10,
         minZoom: 4,
         mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -140,4 +127,44 @@ function showUserMarkerOnMap(x,y,address) {
 
     userLocationMarker.setPosition({ lat: x, lng: y });
     userLocationMarker.setMap(map);
+}
+
+
+function updateCurrentLocation() {
+    var addr = document.getElementById("Location_Address");
+    var xcoordinate = document.getElementById("Location_XCoordinate");
+    var ycoordinate = document.getElementById("Location_YCoordinate");
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            var geocoder = new window.google.maps.Geocoder();             // create a geocoder object
+            var location = new window.google.maps.LatLng(pos.lat, pos.lng);    // turn coordinates into an object          
+            geocoder.geocode({ 'latLng': location }, function (results, status) {
+                if (status === window.google.maps.GeocoderStatus.OK) {           // if geocode success
+                    var detailedLocation = results[0].formatted_address;         // if address found, pass to processing function
+                    addr.value = detailedLocation;
+                    //document.getElementById("Location_Address").value = detailedLocation;
+                    xcoordinate.value = pos.lat;
+                    ycoordinate.value = pos.lng;
+                    userLocationMarker.setTitle(addr);
+                    userLocationMarker.setPosition({ lat: pos.lat, lng: pos.lng });
+                    userLocationMarker.setMap(map);
+                    map.setCenter({ lat: pos.lat, lng: pos.lng });
+                } else {
+                }
+            })
+        }, function () {
+            // handleLocationError(true, myLocationMarker, map.getCenter());
+            alert("Không thể định vị được vị trí của bạn. Bạn cần cho phép trình duyệt sử dụng định vị GPS.");
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        //  handleLocationError(false, myLocationMarker, map.getCenter());
+        alert("Trình duyệt của bạn không hỗ trợ định vị GPS. Vui lòng nâng cấp phiên bản mới nhất của trình duyệt và thử lại sau.");
+    }
 }
