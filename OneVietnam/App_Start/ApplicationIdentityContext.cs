57f2afc1e88c9f7dd195ce9b1322725d98985180
tailmse02457 <tailmse02457@fpt.edu.vn>
2016-08-03 03:34:03 +0700

    Remove related countries file

--- OneVietnam/App_Start/ApplicationIdentityContext.cs
+++ OneVietnam/App_Start/ApplicationIdentityContext.cs
@@ -17,8 +17,7 @@ namespace OneVietnam
     {
         public IMongoCollection<ApplicationUser> Users { get; set; }
         public IMongoCollection<Post> Posts { get; set; }
-        public IMongoCollection<IdentityRole> Roles { get; set; }
-        public IMongoCollection<Country> Countries { get; set; }
+        public IMongoCollection<IdentityRole> Roles { get; set; }        
         public IMongoCollection<DTL.Tag> Tags { get; set; }
         public IMongoCollection<Icon> Icons { get; set; }
 
@@ -26,13 +25,12 @@ namespace OneVietnam
         public IMongoCollection<Message> Messages { get; set; }
 
         private ApplicationIdentityContext(IMongoCollection<ApplicationUser> users, IMongoCollection<Post> posts,
-            IMongoCollection<IdentityRole> roles, IMongoCollection<Country> countries, IMongoCollection<DTL.Tag> tags,
+            IMongoCollection<IdentityRole> roles, IMongoCollection<DTL.Tag> tags,
             IMongoCollection<Icon> icons, IMongoCollection<Report> reports, IMongoCollection<Message> messages)
         {
             Users = users;
             Posts = posts;
-            Roles = roles;
-            Countries = countries;
+            Roles = roles;            
             Tags = tags;
             Icons = icons;
             Reports = reports;
@@ -45,8 +43,7 @@ namespace OneVietnam
             var database = client.GetDatabase(Settings.Default.OneVietnamDatabaseName);
             Users = database.GetCollection<ApplicationUser>("users");
             Posts = database.GetCollection<Post>("posts");
-            Roles = database.GetCollection<IdentityRole>("roles");
-            Countries = database.GetCollection<Country>("countries");
+            Roles = database.GetCollection<IdentityRole>("roles");            
             Tags = database.GetCollection<DTL.Tag>("tags");
             Icons = database.GetCollection<Icon>("icons");
             Reports = database.GetCollection<Report>("reports");
@@ -59,13 +56,12 @@ namespace OneVietnam
             var database = client.GetDatabase(Settings.Default.OneVietnamDatabaseName);
             var users = database.GetCollection<ApplicationUser>("users");
             var posts = database.GetCollection<Post>("posts");
-            var roles = database.GetCollection<IdentityRole>("roles");
-            var countries = database.GetCollection<Country>("countries");
+            var roles = database.GetCollection<IdentityRole>("roles");            
             var tags = database.GetCollection<DTL.Tag>("tags");
             var icons = database.GetCollection<Icon>("icons");
             var reports = database.GetCollection<Report>("reports");
             var messages = database.GetCollection<Message>("messages");
-            return new ApplicationIdentityContext(users, posts, roles, countries, tags, icons, reports, messages);
+            return new ApplicationIdentityContext(users, posts, roles, tags, icons, reports, messages);
         }
         public void Dispose()
         {
