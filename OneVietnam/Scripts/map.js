0b80f53d2640eb9b83fffa2c9e71162f3cc5f377
Dat3T <clever_sheep_93@yahoo.com>
2016-08-05 03:59:55 +0700

    make map move smoothly after searching

--- OneVietnam/Scripts/map.js
+++ OneVietnam/Scripts/map.js
@@ -311,8 +311,37 @@ function checkIfBoundContainPosition(pos) {
         map.setCenter(pos);
     }
     else {
-        map.fitBounds(map.getBounds());
-        map.setCenter(pos);
+     //   map.fitBounds(map.getBounds());
+        if (bounds) {
+            var sw = bounds.getSouthWest();
+            var ne = bounds.getNorthEast();
+
+            var lat1 = sw.lat();
+            var lng1 = sw.lng();
+            var lat2 = ne.lat();
+            var lng2 = ne.lng();
+
+            var dx = (lng1 - lng2) / 2.;
+            var dy = (lat1 - lat2) / 2.;
+            var cx = (lng1 + lng2) / 2.;
+            var cy = (lat1 + lat2) / 2.;
+
+            // work around a bug in google maps...///
+            lng1 = cx + dx / 1.5;
+            lng2 = cx - dx / 1.5;
+            lat1 = cy + dy / 1.5;
+            lat2 = cy - dy / 1.5;
+            /////////////////////////////////////////
+
+            sw = new google.maps.LatLng(lat1, lng1);
+            ne = new google.maps.LatLng(lat2, lng2);
+            bounds = new google.maps.LatLngBounds(sw, ne);
+            map.panTo(pos);
+            map.fitBounds(bounds);
+            map.setCenter(pos);
+
+        }
+
     }
    
 }
