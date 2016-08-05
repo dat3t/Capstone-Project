6798da8a25357b574da2bf3e11ef65de5af29d65
Dat3T <clever_sheep_93@yahoo.com>
2016-08-05 22:50:10 +0700

    fii bug modal

--- OneVietnam/Scripts/map.js
+++ OneVietnam/Scripts/map.js
@@ -806,26 +806,47 @@ function getPostInfo(postID) {
             //   createUserInfoWindowContent(json.UserName, 23, json.Gender, json.Location.Address);
             if (result != '') {
 
-                $("#forModal").empty();
+                $("#postModal").empty();
 
-                $("#forModal").html(result);
+                $("#postModal").html(result);
 
-                $("#forModal").modal('show');
-                history.pushState("../Newsfeed", null, "../Newsfeed/ShowPost?postId=" + postID);
-                $('.carousel').flickity({
-                    // options
-                    cellAlign: 'left',
-                    contain: true
+                $('#postModal').modal({
+                    inverted: true
+                }).modal({
+                    duration:400,
+                    onHide: function () {
+                    }
+                }).modal('show')
+                ;
+                var $carousel = $('.carousel').flickity({
+                    imagesLoaded: true,
+                    percentPosition: false
+                });
+
+                // get transform property
+                var docStyle = document.documentElement.style;
+                var transformProp = typeof docStyle.transform == 'string' ?
+                  'transform' : 'WebkitTransform';
+
+                history.pushState(null, null, "../Newsfeed/ShowPost/" + id);
+                var flkty = $carousel.data('flickity');
+                var $imgs = $('.carousel-cell img');
+                $carousel.on('scroll.flickity', function () {
+                    flkty.slides.forEach(function (slide, i) {
+                        var img = $imgs[i];
+                        var x = (slide.target + flkty.x) * -1 / 3;
+                        img.style[transformProp] = 'translateX(' + x + 'px)';
+                    });
                 });
             }
             window.addEventListener('popstate', function (e) {
-                $("#forModal").modal("hide");
             });
         },
         error: function (xhr, status, error) {
             alert(xhr.responseText);
         }
     });
+   
 }
 
 function showSelectedPostOnMap(Lat, Lng, PostType, PostId, isCallFromPostDetail) {
